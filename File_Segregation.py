bccd98e828e3d430362df9b62cfed5364cd0df77
Tarun Purohit B <89202863+Tarunpurohit146@users.noreply.github.com>
2023-04-23 23:30:37 +0530

    Delete File_Segregation.py

--- File_Segregation.py
+++
@@ -1,8 +0,0 @@
-import os
-ex={'docx':'DOCfile','txt':'Text','pdf':'PDF_file'} #Add more extension based on your choice
-d=os.listdir()
-for i in d:
-   if i.split('.')[-1] in ex:
-   	if not os.path.isdir(ex[i.split('.')[-1]]):
-   		os.mkdir(ex[i.split('.')[-1]])
-   	os.system(f'mv {i} {ex[i.split(".")[-1]]}')
